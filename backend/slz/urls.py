b416599aa3e01f20c629c524aa6fe05a2ac8aa70
adil <adilkhan49@hotmail.com>
2024-09-19 19:21:42 +0500

    change url from statement to statements

--- backend/slz/urls.py
+++ backend/slz/urls.py
@@ -4,6 +4,6 @@ from slz import views
 urlpatterns = [
     re_path(r'posts/$', views.post_list),
     re_path(r'posts/([0-9]+)$', views.post_detail),   
-    re_path(r'statement/$', views.statement_list), 
-    re_path(r'statement/([0-9]+)$', views.statement_detail),    
+    re_path(r'statements/$', views.statement_list), 
+    re_path(r'statements/([0-9]+)$', views.statement_detail),    
 ]
