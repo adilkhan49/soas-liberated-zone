9d516539c0e0d6f5feee0d46b8c4210d67b05103
adil <adilkhan49@hotmail.com>
2024-10-04 00:54:02 +0500

    mobile menu working

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 import { Outlet, Link } from "react-router-dom";
 import { useContext } from 'react'
 import AuthContext from "../context/AuthContext";
 
+
+
+
+
 export default function Root() {
   
   let { user, logoutUser } = useContext(AuthContext)
-
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  // function toggleMenu() {
+  //   const menu = document.querySelector("#mobile-menu")
+  //   if (menu.classList.contains('hidden')) {
+  //       menu.classList.remove('hidden');
+  //   } else {
+  //       menu.classList.add('hidden');
+  //   }
+  // }
 
   return (
 <div class="min-h-screen bg-gradient-to-b from-green-700 to-green-700">
@@ -13,7 +27,9 @@ export default function Root() {
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          onClick={() => setIsNavOpen((prev) => !prev)}
+          >
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
             <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -69,19 +85,25 @@ export default function Root() {
       </div>
   
   </div>
-
-  {/* <div class="sm:hidden" id="mobile-menu">
-    <div class="space-y-1 px-2 pb-3 pt-2">
-      <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-      <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-      <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-      <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+  
+  <div class={!isNavOpen && "hidden"}>
+    <div class="sm:hidden" id="mobile-menu">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        {/* <a href="calendar" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Calendar</a> */}
+        <a href="/" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline">Home</a>
+        <a href="getinvolved" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline">Get Involved</a>
+        <a href="calendar" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline">Calendar</a>
+        <a href="journal" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline">Journal</a>
+        <a href="fundraiser" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline">Fundraiser</a>
+        <a href="about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white no-underline">About</a>
+      </div>
     </div>
-  </div> */}
+  </div>
 </nav>
          <div class="mx-10 my-5">
            <Outlet />
 </div>
+
 </div>
 
   );
