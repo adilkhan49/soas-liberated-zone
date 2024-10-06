e1ea1a9b2a1064b61b184b50d166a75babb83941
adil <adilkhan49@hotmail.com>
2024-10-07 01:58:28 +0500

    api url windows origiin

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,5 +1,14 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
+// const API_URL = import.meta.env.VITE_API_URL;
+var API_URL = '';
+if (window.location.origin === "http://localhost:5173") {
+    API_URL = API_URL + "http://localhost:8000/api/";
+  } else {
+    API_URL = API_URL + window.location.origin;
+  }
+  console.log('hello origin')
+console.log(window.location.origin);
+console.log(API_URL)
+  
 export const POSTS_API_URL = API_URL + "posts/";
 export const STATEMENTS_API_URL =  API_URL + "statements/";
 export const EVENTS_API_URL =  API_URL + "events/";
