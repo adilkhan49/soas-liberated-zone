1226b2ccb66329b584142f350e890a5e1cd5109c
adil <adilkhan49@hotmail.com>
2024-10-30 14:44:39 -0600

    updates to about page

--- frontend/src/routes/about.jsx
+++ frontend/src/routes/about.jsx
@@ -23,41 +23,40 @@ class About extends Component {
 
     render() {
       return (
-        <div>
+        <div class="m-5">
           <h1>About</h1>
-          <h2 class='font-tourney'>Demands</h2>
+          <h2 class='font-tourney text-5xl'>Demands</h2>
 
-          <p class= "container bg-white p-10 border-2 border-black ">
-            These are our demands to SOAS University:
-            <br></br>
+          <p class= "flex container min-w-screen bg-amber-50 text-black mx-auto sm:text-2xl p-10 border-2 border-red-700  ">
 
-            <ol class="list-decimal list-inside text-justify">
+
+            <ol class="list-none list-inside text-justify space-y-5">
               <li>
-                <b>Disclose</b> details of all University investments immediately and continue to do so on an annual basis.
+                <span class="text-xl sm:text-3xl font-bold text-red-700">Disclose</span> details of all University investments immediately and continue to do so on an annual basis.
               </li>
               <li>
-                <b>Divest</b> from companies complicit in Israel’s occupation and denial of Palestinian rights, including Accenture, Albemarle, Alphabet, Barclays, Microsoft, Newton Investment Management, RELX, and Sony, and commit to not reinvesting.
+              <span class="text-xl sm:text-3xl font-bold text-red-700">Divest</span> from companies complicit in Israel’s occupation and denial of Palestinian rights, including Accenture, Albemarle, Alphabet, Barclays, Microsoft, Newton Investment Management, RELX, and Sony, and commit to not reinvesting.
               </li>
               <li>
-                <b>Terminate</b> the university’s banking and lending arrangement with Barclays.
+                <span class="text-xl sm:text-3xl font-bold text-red-700">Terminate</span> the university’s banking and lending arrangement with Barclays.
               </li>
               <li>
-                <b>Cut ties</b> with University of Haifa and boycott all Israeli academic institutions, which are complicit in the genocidal campaign on Gaza and in widespread violation of Palestinian rights.
+               <span class="text-xl sm:text-3xl font-bold text-red-700">Cut ties</span> with University of Haifa and boycott all Israeli academic institutions, which are complicit in the genocidal campaign on Gaza and in widespread violation of Palestinian rights.
               </li>
               <li>
-                <b>Commit</b> to supporting Palestinian education and the rebuilding of Gaza’s destroyed schools, hospitals, and universities.  Establish partnerships and exchanges with Palestinian institutions and academics, increase scholarships for Palestinian students, and advocate for the removal of restrictions on Palestinian expression and movement.
+                <span class="text-xl sm:text-3xl font-bold text-red-700">Commit</span> to supporting Palestinian education and the rebuilding of Gaza’s destroyed schools, hospitals, and universities.  Establish partnerships and exchanges with Palestinian institutions and academics, increase scholarships for Palestinian students, and advocate for the removal of restrictions on Palestinian expression and movement.
               </li>
               <li>
-                <b>Guarantee</b> the rights of students and staff to free expression and end the targeted repression of Palestine solidarity activism on campus.  Revoke the new SOAS protest policy.
+                <span class="text-xl sm:text-3xl font-bold text-red-700">Guarantee</span> the rights of students and staff to free expression and end the targeted repression of Palestine solidarity activism on campus.  Revoke the new SOAS protest policy.
               </li>
               <li>
-                <b>Advocate</b> for the UK government to implement an immediate arms embargo on Israel and to use all leverage to effect an immediate, unconditional, and permanent ceasefire
+                <span class="text-xl sm:text-3xl font-bold text-red-700">Advocate</span> for the UK government to implement an immediate arms embargo on Israel and to use all leverage to effect an immediate, unconditional, and permanent ceasefire
               </li>      
             </ol>
           </p>
 
           <h2 class='font-tourney'>Timeline</h2>
-          <div class = "container bg-white p-10 border-2 border-black">
+          <div class = "container bg-amber-50 p-10 border-2 border-black">
             <Timeline
               events={this.state.timeline_events}
           />
