d0d724bc7a31df6c1f956debde18e701fd1d5b5d
adil <adilkhan49@hotmail.com>
2024-10-30 14:29:26 -0600

    carousel across page and add ig back

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -22,7 +22,7 @@ const Carousel = ({ images, autoSlide = true, autoSlideInterval = 10000 }) => {
     // relative w-full max-w-xl mx-auto
     // overflow-hidden relative h-64
   return (
-    <div className=" relative object-scale-down w-full min-h-screen overflow-hidden">
+    <div className=" relative object-scale-down w-full  h-full overflow-hidden">
       <div className="">
         {images.map((image, index) => (
           <div
@@ -31,7 +31,7 @@ const Carousel = ({ images, autoSlide = true, autoSlideInterval = 10000 }) => {
               index === currentIndex ? 'translate-x-0' : 'translate-x-full'
             }`}
           >
-            <img src={image.url} alt={`Slide ${index}`} className="w-full h-full object-cover" />
+            <img src={image.url} alt={`Slide ${index}`} className="w-full h-full mx-auto object-cover" />
           </div>
         ))}
       </div>
@@ -82,6 +82,15 @@ const Carousel = ({ images, autoSlide = true, autoSlideInterval = 10000 }) => {
           />
         ))}
       </div>
+      <form action={'/about'} method="get" target={'target'}>
+        <button
+          // className="absolute bottom-0 left-0 transform -translate-y-1/2  translate-x-1/2 rounded-full select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center "
+          className="bg-black text-5xl border-gray-700 hover:bg-gray-700 text-white px-4 rounded-full absolute bottom-0 left-0 transform -translate-y-20 text-2xl  translate-x-1/2 rounded-full select-none transition-all   text-white grid place-items-center "
+          onClick={prevSlide}
+        >
+          Our Demands
+        </button>
+      </form>
     </div>
   );
 };
