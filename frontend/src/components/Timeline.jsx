403668cd19f5d32eec0dd2fe1ce808f4d67d931e
adil <adilkhan49@hotmail.com>
2024-11-02 09:42:08 -0600

    timeline exclude date or month frontend

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -5,7 +5,7 @@ function Timeline({events}) {
     const listing= events.map((event) => (
         <div class="mb-6 ms-4">
             <div class="absolute w-3 h-3 bg-green-800 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-black"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-red-800">{formatDate(event.timeline_date)}</time>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-red-800">{formatDate(event.timeline_date,event.exclude_date,event.exclude_month)}</time>
             <p class="text-lg text-gray-900">{event.title}</p>
         </div>));
 
