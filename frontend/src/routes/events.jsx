cd8419defb50d1e98031c8b0aaadb2ca44d78cc4
adil <adilkhan49@hotmail.com>
2024-11-02 01:16:49 -0600

    calendar background full width

--- frontend/src/routes/events.jsx
+++ frontend/src/routes/events.jsx
@@ -4,8 +4,10 @@ import Calendar from "../components/calendar";
 export default function Events() {
   
   return (
-    <div class ="container bg-green-500 opacity-80">
-      <Calendar/>
+    <div class="bg-green-500 opacity-80">
+      <div class ="pt-10">
+        <Calendar/>
+      </div>
     </div>
   );
 }
