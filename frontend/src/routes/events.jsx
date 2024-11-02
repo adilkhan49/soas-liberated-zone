90143b534ed71d47a9278d6421c59587d6938ddb
adil <adilkhan49@hotmail.com>
2024-11-02 01:32:31 -0600

    add message to calendar page

--- frontend/src/routes/events.jsx
+++ frontend/src/routes/events.jsx
@@ -5,6 +5,9 @@ export default function Events() {
   
   return (
     <div class="bg-green-500 opacity-80">
+      <div class='container bg-green-300 text-center min-w-full text-green-800 text-xl text-bold p-3'>
+  Don't miss out on events. Join our broadcast!"
+      </div>
       <div class ="pt-10">
         <Calendar/>
       </div>
