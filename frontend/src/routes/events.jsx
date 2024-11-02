a3ed6e520c515661d199880a3f064820d65bd581
adil <adilkhan49@hotmail.com>
2024-11-02 01:49:07 -0600

    remove double quote

--- frontend/src/routes/events.jsx
+++ frontend/src/routes/events.jsx
@@ -6,7 +6,7 @@ export default function Events() {
   return (
     <div class="bg-green-500 opacity-80">
       <div class='container bg-green-300 text-center min-w-full text-green-800 text-xl text-bold p-3'>
-  Don't miss out on events. Join our broadcast!"
+  Don't miss out on events. Join our broadcast!
       </div>
       <div class ="pt-10">
         <Calendar/>
