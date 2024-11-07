5345d6ef191acd2d9c17db2d78251387897aa169
adil <adilkhan49@hotmail.com>
2024-11-07 16:23:45 -0600

    Add Jehad picture and text

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -25,25 +25,29 @@ class Home extends Component {
 
   render() {
     return (
-      <div
-      // class = "w-full bg-repeat border-2e bg-[url('/src/hariya-tile-lg.svg')] sm:bg-[url('/src/hariya-tile.svg')]"
-      >
+      <div>
         <div class="flex justify-center  h-[80vh] ">
           <Carousel images={this.state.carousel_images} />
         </div>
 
-        <p class='container font-kanit font-bold italic bg-amber-50 p-5 bg-green-700 sm:bg-transparent text-gray-50 sm:text-black mx-auto  text-lg sm:text-3xl text-center text-italic'>The SOAS liberated zone is a student led site of anti colonial struggle for Gaza against SOAS university’s complicity in the ongoing genocide. We work to end SOAS’s living colonial legacy to become an institution in service of people’s liberation and a truly decolonial education.</p>
+        <p class='container text-white font-kanit font-bold italic bg-amber-50 p-10 bg-green-700 sm:bg-transparent text-gray-50 sm:text-black mx-auto  text-lg sm:text-3xl text-center text-italic'>The SOAS liberated zone is a student led site of anti colonial struggle for Gaza against SOAS university’s complicity in the ongoing genocide. We work to end SOAS’s living colonial legacy to become an institution in service of people’s liberation and a truly decolonial education.</p>
 
+        <div class="flex grid grid-cols-1 sm:grid-cols-2 h-1/4 ">
 
+          <div class=" h-auto border-none">
+            <figure>
+              <img class="" src={"https://i.postimg.cc/HkSY3qRW/jehad-1.jpg"} alt="Intifada to Victory" />
+              {/* <a href="/login"><figcaption class="px-5 py-3 text-center text-lg text-white font-semibold underline ">!انتفاضة شامیے</figcaption></a> */}
+            </figure>
+          </div>
 
-
-        {/* <div class="flex justify-center items-center border-none">
-          <figure>
-            <img class="h-auto max-w-full rounded-lg " src={homepageimg} alt="Intifada to Victory" />
-            <a href="/login"><figcaption class="px-5 py-3 text-center text-lg text-white font-semibold underline ">!انتفاضة شامیے</figcaption></a>
-          </figure>
+          <div>
+            <p class='flex container h-full inline-block align-middle justify-center font-kanit font-bold italic bg-amber-50 p-10 bg-green-700 sm:bg-transparent text-gray-50 sm:text-black mx-auto  text-lg sm:text-2xl text-center text-italic '>
+              The situation in Gaza is dire, but our spirit remains unbroken. Through countless hardships, from blockades to bombings, the people of Gaza continue to strive for a life of dignity, freedom, and peace.
+	 Your support and solidarity matter more than words can express. It's not just about standing with Gaza; it's about standing for justice, human rights, and dignity everywhere. Now.</p>
+          </div>
         </div>
-        */}
+        
         <div class="">
           <InstagramEmbed url="https://www.instagram.com/soasliberatedzone/"  />
         </div> 
