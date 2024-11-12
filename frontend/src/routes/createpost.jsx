c9dbe539bda81f70fcc68e9d940d9a65094a37d4
adil <adilkhan49@hotmail.com>
2024-11-11 19:16:57 -0600

    remove create post line

--- frontend/src/routes/createpost.jsx
+++ frontend/src/routes/createpost.jsx
@@ -4,9 +4,6 @@ import Editor from '../components/createPost';
 function CreatePost () {
     return (
         <div>
-            <h1>
-                Create Post
-            </h1>
             <Editor />
         </div>
     )
