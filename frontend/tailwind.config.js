34c86b7d0bafb7b35e570ed1c632afcc8d248fa4
adil <adilkhan49@hotmail.com>
2024-11-16 11:37:40 -0600

    custom border widths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,7 +21,18 @@ export default {
       bold: '700',
       extrabold: '800',
       black: '900',
-    }
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '5': '5px',
+      '6': '6px',
+      '7': '7px',
+      '8': '8px',
+    },
   },
   plugins: [],
 }
