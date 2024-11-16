8413daa451512ef60f6678e19fbb660d77dc0448
adil <adilkhan49@hotmail.com>
2024-11-16 11:38:18 -0600

    flipcountdown

---
+++ frontend/src/components/flipcountdown.jsx
@@ -0,0 +1,33 @@
+function FlipCountdown({number=0,caption='',top_color='bg-black',bottom_color='bg-black', border_color='border-green-700', line_color='bg-green-700', opacity='opacity-75'}) {
+
+    const string_array = number.toString().split('');
+    const width = 'w-'+((string_array.length)+1*20).toString();
+    return (
+    <div class='flex flex-col justify-center items-center z-20 '>
+        <div class="flex flex-row gap-1">
+            {string_array.map(digit => (
+                <div class={`relative w-16 sm:w-20 font-mono font-extrabold items-center text-5xl sm:text-7xl text-white shadow-2xl gap-x-px border-6 ${border_color} ${opacity} rounded-3xl`}>
+                    <div class="relative py-2 sm:py-4 rounded-2xl ">
+                        <div class="absolute inset-0 w-full h-full grid grid-rows-2 ">
+                        <div class={`${top_color} ${opacity} rounded-t-2xl`}></div>
+                        <div class={`${bottom_color} ${opacity} rounded-b-2xl`}></div>
+                    </div>
+                    <div class="relative text-center">{digit}</div>
+                        <div class="absolute inset-0 w-full h-full flex items-center justify-center">
+                            <div class={`h-0.5 w-full ${line_color}`}></div>
+                        </div>
+                    </div>
+                </div>
+                )
+            )   
+        }
+
+        </div>
+        <div class={`$ h-20 font-anton text-center text-2xl sm:text-4xl font-bold font-outline-1 )]`}>
+            {caption}
+        </div>
+    </div>
+  );  
+}
+
+export default FlipCountdown;
