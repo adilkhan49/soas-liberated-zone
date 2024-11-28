6263eec2366b38a1c4ca3ab6b3b0683aaa629ad7
adil <adilkhan49@hotmail.com>
2024-11-28 10:58:59 -0600

    updates to home page

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -6,6 +6,7 @@ import { CAROUSEL_IMAGES_API_URL } from "../constants";
 import FlipCountdown from "../components/flipcountdown";
 import logo from "/src/slz-logo.png";
 import { Link } from "react-router-dom";
+import Subscribe from "../components/SubscribeForm";
 
 class Home extends Component {
 
@@ -33,14 +34,13 @@ class Home extends Component {
         <div class="flex justify-center  h-[80vh] ">
           <Carousel images={this.state.carousel_images} />
         </div>
-        <div class=' font-tourney  bg-black bg-opacity-85 hover:bg-opacity-50 text-opacity-100 text-left text-2xl sm:text-5xl p-3 sm:px-14'>
+        <div class=' font-tourney  bg-black bg-opacity-85 hover:bg-opacity-50 text-left text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-3 sm:px-14'>
           <Link to='/getinvolved' class="no-underline text-green-900 font-bold">
-            {"End the complicity. End the repression. SOAS must become an institution in service of its community >>>"}
-          </Link>
+          The SOAS liberated zone is a student led site of anti colonial struggle for Gaza against SOAS university’s complicity in the ongoing genocide. We work to end SOAS’s living colonial legacy to become an institution in service of people’s liberation and a truly decolonial education.          </Link>
         </div>
 
-        <div class='relative flex flex-col justify-center min-w-full container py-10 px-4 sm:p-14 pb-0 text-red-800  text-lg sm:text-4xl text-center  text-italic justify-center '>
-          <div class='z-10 relative flex justify-center min-w-full container font-kanit font-bold italic p-2 sm:py-5 sm:px-32  bg-amber-50 text-red-800  text-lg sm:text-4xl text-center text-italic justify-center shadow-2xl border-4 border-red-800 leading-normal'>
+        <div class='relative flex flex-col justify-center min-w-full container py-10 px-4 md: p-4 lg:p-14 pb-0 text-red-800  text-lg sm:text-2xl md:text-4xl text-center  text-italic justify-center '>
+          <div class='z-10 relative flex justify-center min-w-full container font-kanit font-bold italic p-2 md:py-5 sm:px-12  md:px-32  bg-amber-50 text-red-800  text-lg sm:text-4xl text-center text-italic justify-center shadow-2xl border-4 border-red-800 leading-normal'>
             The SOAS liberated zone is a student led site of anti colonial struggle for Gaza against SOAS university’s complicity in the ongoing genocide.  We cannot accept forced complicity in crimes against humanity and antidemocratic repression in our community. That is why we work to end SOAS’s living colonial legacy to become an institution in service of people’s liberation and a truly decolonial education. 
             {/* <div class= "sm:h-96 hidden sm:block absolute w-1/4 pt-5 sm:p-10 sm:h-96 container justify-center left-0 translate-y-32 z-20">
               <img src = {logo}/>
@@ -48,26 +48,68 @@ class Home extends Component {
           </div>
 
 
-          <div className="sm:flex sm:flex-row gap-28 ">
-            <div class= "hidden sm:block w-1/4 pt-5 container justify-center ">
+          <div className="lg:flex lg:flex-row gap-28 ">
+            <div class= "hidden lg:block w-1/4 pt-5 container justify-center ">
               <img src = {logo}/>
             </div>
             <div class=' grid grid-cols-1 gap-y-3  p-6 font-kanit font-bold italic   text-lg sm:text-4xl text-center items-center'>
-                <Link to='/about#demands' state={{ demands: true, statements: false, timeline: false }}class="w-60 sm:w-96 p-4 border border-green-800 bg-white text-green-800 shadow-2xl rounded-3xl m-auto hover:bg-amber-50 no-underline hover:underline -translate-x-10">
+                <Link to='/about#demands' state={{ demands: true, statements: false, timeline: false }}class="w-60 sm:w-96 p-4 border border-green-800 bg-white text-green-800 shadow-2xl rounded-3xl m-auto hover:bg-amber-50 no-underline hover:underline lg:-translate-x-10 xl:-translate-x-10">
                   {"Our Demands >>>"}
                 </Link>
-                <Link to='/about#statements' state={{ demands: false, statements: true, timeline: false }} class="w-60 sm:w-96 p-4 border border-green-800 bg-white text-green-800 shadow-2xl rounded-3xl m-auto hover:bg-amber-50 no-underline hover:underline sm:translate-x-40 ">
+                <Link to='/about#statements' state={{ demands: false, statements: true, timeline: false }} class="w-60 sm:w-96 p-4 border border-green-800 bg-white text-green-800 shadow-2xl rounded-3xl m-auto hover:bg-amber-50 no-underline hover:underline lg:translate-x-20 xl:translate-x-40 ">
                   {"Our Statements >>>"}
                 </Link>
-                <Link to='/about#timeline' state={{ demands: false, statements: false, timeline: true }} class="w-60 sm:w-96 p-4 border border-green-800 bg-white text-green-800 shadow-2xl rounded-3xl m-auto hover:bg-amber-50 no-underline hover:underline translate-x-10 sm:translate-x-80">
+                <Link to='/about#timeline' state={{ demands: false, statements: false, timeline: true }} class="w-60 sm:w-96 p-4 border border-green-800 bg-white text-green-800 shadow-2xl rounded-3xl m-auto hover:bg-amber-50 no-underline hover:underline lg:translate-x-40 xl:translate-x-80">
                 {"Our Story >>>"}
                 </Link>
             </div>
           </div>
         </div>
 
+        <div class='relative h-[80vh] my-5 '>
+          <div class="relative h-full m-5 rounded-3xl bg-lime-900 bg-opacity-75">
+              <div class="relative h-full mr-5 md:mr-20 ml-20 ">
+                <img 
+                  class="absolute h-full right-0 object-cover" 
+                  src={"https://i.postimg.cc/25nW03N0/get-involved.jpg"} 
+                />
+                <div class="absolute top-0 right-0 left-0 top-4  w-1/2 xl:w-1/3 rounded-3xl text-black border-10 bg-amber-50 border-green-800 text-center py-4 sm:py-4 lg:py-12 align-middle   sm:space-y-4 
+                  bg-repeat md:bg-[url('/src/hariya-tile-lg-light.svg')]
+                  ">
+                  <div class="font-anton font-bold text-lg md:text-2xl lg:text-3xl xl:text-4xl text-green-800 tracking-widest">
+                    UPCOMING EVENTS
+                  </div>
+                  <div >
+                    <ul class="font-anton text-lg lg:text-xl xl:text-2xl text-center sm:space-y-4">
+                      <li>1/12 Trump is trampled</li>
+                      <li>10/12 Netanyahu is arrested</li>
+                      <li>12/12 Adam Habib is fired</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
 
-        <div class="flex grid grid-cols-1 sm:grid-cols-2 ">
+        <div class="my-10 mx-2 mx-10 sm:mx-20 md:mx-20 lg:mx-40">
+          <div class="text-red-800 font-kanit text-5xl font-bold">
+            STAY IN TOUCH
+          </div>
+          <form
+            action="https://signal.group/#CjQKIM5LoPsS4lKVkHR08Nr686ZxJRWhm_hImbG3bNpJbJglEhCuU5LbAmsdAiz-oQgKH6lp"
+            target="_blank"
+            class="container text-black md:text-xl lg:text-3xl font-anton p-2 m-2 mx-auto w-2/3 xl:w-1/2 lg:-translate-x-20  tracking-widest bg-amber-50 border-red-800 hover:bg-red-800 hover:border-black hover:text-amber-50 rounded-full border-10 items-center text-center">
+            <button>join the whatsapp broadcast </button>
+          </form>
+          <div
+            class="container text-black md:text-xl lg:text-3xl font-anton p-2 m-2 mx-auto w-2/3 xl:w-1/2  tracking-widest bg-amber-50 border-red-800 rounded-full border-10 items-center text-center">
+            <div>subscribe for email updates </div>
+          </div>
+          <Subscribe />
+        </div>
+
+
+        <div class="flex grid grid-cols-1 lg:grid-cols-2 ">
 
           <div class=" h-auto border-none">
             <figure>
