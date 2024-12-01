0cb647ef9dbc0f29bf79df7a38ef345bf1dce21c
adil <adilkhan49@hotmail.com>
2024-12-01 02:26:21 -0600

    graphic design changes to democractise education

--- frontend/src/routes/democratiseeducation.jsx
+++ frontend/src/routes/democratiseeducation.jsx
@@ -33,17 +33,17 @@ function DemocratiseEducation () {
         </div>
       </div>
 
-      <div class='flex items-center gap-20 font-anton bg-black bg-opacity-75  text-opacity-100 text-white text-left text-2xl sm:text-4xl p-3 sm:px-14'>
+      <div class='flex items-center gap-20 font-leaguespartan bg-black bg-opacity-65  text-opacity-100 text-white text-left text-2xl sm:text-4xl p-3 sm:px-14'>
           <img class="hidden lg:block object-fit h-72 pt-10 pb-10 " src="https://images2.imgbox.com/ea/4f/X5dVV8cZ_o.png"/>
           <div class = "m-auto text-opacity-100 text-center">
             We are launching the Democratise Education: End SOAS Repression campaign. To bring together students, staff, and our communities, as we defend our university’s democracy against increasingly repressive attacks. Will you join us? 
           </div>
       </div>
 
-      <div class="flex flex-col gap-2 sm:gap-5 font-anton text-white text-xl sm:text-3xl p-8 lg:p-20 lg:px-64">
-        <a href='#state-of-repression' class="text-white no-underline sm:indent-0"><span class=" text-3xl sm:text-7xl text-green-800">⧨ </span>The state of repression at SOAS</a>
-        <a href='#initial-demands' class="text-white no-underline sm:indent-8 xl:indent-40"><span class="text-3xl sm:text-7xl text-green-800">⧨ </span>Initial demands of the campaign</a>
-        <a href='#work-to-democratise-education' class="text-white no-underline sm:indent-16 xl:indent-80"><span class="text-3xl sm:text-7xl text-green-800">⧨ </span>Work to Democratise Education</a>
+      <div class="flex flex-col gap-2 sm:gap-5 font-leaguespartan font-bold text-white text-xl sm:text-3xl lg:text-4xl xl:text-5xl p-8 lg:p-10 xl:px-20">
+        <a href='#state-of-repression' class="hover:text-red-800 font-outline-2-black text-white no-underline sm:indent-0">The state of repression at SOAS <span class="text-3xl sm:text-4xl text-red-800">▶</span></a>
+        <a href='#initial-demands' class="hover:text-red-800 font-outline-2-black text-white no-underline sm:indent-8 xl:indent-40">Initial demands of the campaign <span class="text-3xl sm:text-4xl text-red-800">▶</span></a>
+        <a href='#work-to-democratise-education' class="hover:text-red-800 font-outline-2-black text-white no-underline sm:indent-16 xl:indent-80">Work to Democratise Education <span class="text-3xl sm:text-4xl text-red-800">▶</span></a>
       </div>
       
       <div
@@ -55,23 +55,23 @@ function DemocratiseEducation () {
         <div class="pb-10">
           In the last few years SOAS management has steadily been creating an increasingly repressive environment at the university for both students and workers. Nevertheless the academic year of 23/24 has seen new heights of authoritarian crackdown with a truly alarming response to the growing student movement Palestine. Since the academic year 2023/2024:
         </div>
-        <div class="pb-10">
-          11 students have been suspended;
+        <div class="pb-10 text-red-500">
+          - 11 students have been suspended;
+        </div>
+        <div class="pb-10 text-red-500">
+          - One has been excluded;
+        </div>
+        <div class="pb-10 text-red-500">
+          - At least 6 students received final warnings for protestsl;
+        </div>
+        <div class="pb-10 text-red-500">
+          - 2 out of 4 of the student union representatives have been dismissed for reasons relating to protest. 
+        </div>
+        <div class="pb-10 text-red-500">
+          - Most recently University of London has taken out a high court injunction to ban all pro-Palestine activism on campus. An unprecedented attack on advocacy within the university.
         </div>
         <div class="pb-10">
-          One has been excluded;
-        </div>
-        <div class="pb-10">
-          At least 6 students received final warnings for protestsl;
-        </div>
-        <div class="pb-10">
-          2 out of 4 of the student union representatives have been dismissed for reasons relating to protest. 
-        </div>
-        <div class="pb-10">
-          Most recently University of London has taken out a high court injunction to ban all pro-Palestine activism on campus. An unprecedented attack on advocacy within the university.
-        </div>
-        <div class="pb-10">
-        The state of basic advocacy rights at SOAS is chilling, with detrimental impacts on workers' struggle at the university. We have recently seen blatent union victimisation, 100% pay cuts for lecturers taking strike action,  increasing decline in working conditions for cleaners, and a ban on the picket line at the entrance of SOAS, its historical location for decades.
+        The state of basic advocacy rights at SOAS is chilling, with detrimental impacts on workers' struggle. We have recently seen blatent union victimisation, 100% pay cuts for lecturers taking strike action,  increasing decline in working conditions for cleaners, and a ban on the picket line at the entrance of SOAS, its historical location for decades.
         </div>
         <div class="pb-10">
           This repression has also taken place within wider institutional reforms such as new  protest guidelines that ban effective protest on campus, as well as a new constitution in the student union that has put in place external trustees in a previously independent, student-run union. All of this has been overseen by Adam Habib, a director who has recieved a vote of no confidence from all three unions at SOAS.  
@@ -81,28 +81,42 @@ function DemocratiseEducation () {
         </div>
       </div>
 
-      <div id='initial-demands' 
-        class='flex flex-col font-kanit text-white bg-opacity-75 p-4 lg:p-24 text-xl sm:text-3xl sm:m-8 rounded-3xl m-4 lg:mx-48 lg:mx-64 text-center'
-      >
-        <div class='text-3xl sm:text-5xl pb-10'>
-          Initial Demands
+      <div class='py-5 sm:py-10'>
+        <div class= "flex h-48 sm:h-96 container min-w-full justify-center">
+            <img
+              src = 'https://images2.imgbox.com/9f/aa/7qVqi0UC_o.png'
+            />
         </div>
-        <div>
-        The campaign has these few initial demands, which are the bare minimum to ensure basic democracy within SOAS. 
+      </div>
+
+      <div id='initial-demands' class='flex flex-col font-anton  bg-black text-white bg-opacity-65 gap-y-10 font-leaguespartanfont-bold text-black text-3xl sm:text-5xl lg:text-6xl xl:text-7xl pb-10 text-left align-middle justify-evenly'>
+          <div class="mt-4 indent-20 text-red-500 font-outline-4-black ">Initial Demands</div>
+          <div class='text-red-500 font-leaguespartan flex flex-col  font-bold   px-4 lg:px-24 text-xl sm:text-3xl sm:mx-8 rounded-3xl  text-left'>
+          The campaign has these few initial demands, the bare minimum to ensure basic democracy within SOAS. 
+        </div>
+          
         </div>
 
-        <div class="flex flex-col gap-2 sm:gap-5  text-xl sm:text-3xl p-8 ">
-          <div><span class=" text-3xl sm:text-5xl text-green-800">⧨ </span>
-            Reinstate our SU co-Presidents Abel and Alex
+        {/* <div class='flex flex-col font-kanit text-white bg-opacity-75 px-4 lg:px-24 text-xl sm:text-3xl sm:mx-8 rounded-3xl m-4 lg:mx-48 lg:mx-64 text-center' */}
+      <div class='flex flex-col font-kanit font-bold  text-black bg-opacity-75 px-4 lg:px-24 text-xl sm:text-3xl sm:mx-8 rounded-3xl  text-center'>
+
+
+        <div class="flex flex-col gap-y-4 m-4 lg:mx-24 lg:mx-64 sm:gap-5  text-xl sm:text-3xl md:text-4xl lg:text-5xl p-8 text-black ">
+        <div><span class=" text-3xl sm:text-5xl text-red-800">▼ </span><span class="font-outline-2-red-800">
+          Reinstate our SU co-Presidents Abel and Alex
+          </span> 
           </div>
-          <div><span class=" text-3xl sm:text-5xl text-green-800">⧨ </span>
+          <div><span class=" text-3xl sm:text-5xl text-red-800">▼ </span><span class="font-outline-2-red-800">
             Drop the ongoing suspensions six month suspensions against Haya and Tara
+            </span>
           </div>
-          <div><span class=" text-3xl sm:text-5xl text-green-800">⧨ </span>
+          <div><span class=" text-3xl sm:text-5xl text-red-800">▼ </span><span class="font-outline-2-red-800">
             Drop the final warnings against four students for participating in peaceful protest 
+            </span>
           </div>
-          <div><span class=" text-3xl sm:text-5xl text-green-800">⧨ </span>
+          <div><span class=" text-3xl sm:text-5xl text-red-800">▼ </span><span class="font-outline-2-red-800">
             Scrap the authoritarian ‘Protest events at SOAS - guidelines for students’
+            </span>
           </div>
         </div>
 
@@ -110,7 +124,7 @@ function DemocratiseEducation () {
 
       <div 
         id='work-to-democratise-education' 
-        class='flex flex-col font-kanit bg-black text-white bg-opacity-75 p-4 lg:p-24 text-xl sm:text-3xl sm:m-8 rounded-3xl m-4 lg:mx-48 lg:mx-64 text-center'
+        class='flex flex-col font-kanit bg-black text-white bg-opacity-65 p-4 lg:p-24 text-xl sm:text-3xl sm:m-8 rounded-3xl m-4 lg:mx-48 lg:mx-64 text-center'
       >
         <div   class='text-3xl sm:text-5xl pb-10'>
           Work to Democratise Education
@@ -126,7 +140,7 @@ function DemocratiseEducation () {
       </div>
 
       <div class='text-center font-bold text-3xl sm:text-4xl font-kanit md:py-12 md:px-36'>
-        SOAS can be an institution <span class='text-white'>FOR</span> the community <span class='text-white'>BY</span> the community, one we can be proud of, but only through our collective struggle  
+        SOAS can be an institution <span class=''>FOR</span> the community <span class=''>BY</span> the community, one we can be proud to work and study at, but only through our collective struggle  
       </div>
       <img
         class='h-[100vh] w-full object-cover object-bottom'
