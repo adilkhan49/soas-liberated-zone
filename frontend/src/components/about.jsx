9b7fa254b4127f1f3081b36241e1185ff21ebf88
adil <adilkhan49@hotmail.com>
2024-12-01 22:23:46 -0600

    demands smaller

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -33,28 +33,28 @@ class AboutComponent extends Component {
     };
 
     demands = (
-        <div class= "flex container px-8 md:px-12 lg:px-48 py-6 sm:py-14 min-w-full justify-center text-white bg-black bg-opacity-70  text-xl sm:text-2xl lg:text-3xl xl:text-4xl   ">
+        <div class= "flex container px-8 md:px-12 lg:px-48 py-6 sm:py-14 min-w-full justify-center text-white bg-black bg-opacity-70  text-lg sm:text-xl lg:text-2xl xl:text-3xl   ">
           <ol class="list-none list-inside text-justify space-y-5">
             <li>
-              <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Disclose</span> details of all University investments immediately and continue to do so on an annual basis.
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Disclose</span> details of all University investments immediately and continue to do so on an annual basis.
             </li>
             <li>
-             <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Divest</span> from companies complicit in Israel’s occupation and denial of Palestinian rights, including Accenture, Albemarle, Alphabet, Barclays, Microsoft, Newton Investment Management, RELX, and Sony, and commit to not reinvesting.
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Divest</span> from companies complicit in Israel’s occupation and denial of Palestinian rights, including Accenture, Albemarle, Alphabet, Barclays, Microsoft, Newton Investment Management, RELX, and Sony, and commit to not reinvesting.
             </li>
             <li>
-              <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Terminate</span> the university’s banking and lending arrangement with Barclays.
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Terminate</span> the university’s banking and lending arrangement with Barclays.
             </li>
             <li>
-              <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Cut ties</span> with University of Haifa and boycott all Israeli academic institutions, which are complicit in the genocidal campaign on Gaza and in widespread violation of Palestinian rights.
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Cut ties</span> with University of Haifa and boycott all Israeli academic institutions, which are complicit in the genocidal campaign on Gaza and in widespread violation of Palestinian rights.
             </li>
             <li>
-              <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl  font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Commit</span> to supporting Palestinian education and the rebuilding of Gaza’s destroyed schools, hospitals, and universities.  Establish partnerships and exchanges with Palestinian institutions and academics, increase scholarships for Palestinian students, and advocate for the removal of restrictions on Palestinian expression and movement.
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl  font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Commit</span> to supporting Palestinian education and the rebuilding of Gaza’s destroyed schools, hospitals, and universities.  Establish partnerships and exchanges with Palestinian institutions and academics, increase scholarships for Palestinian students, and advocate for the removal of restrictions on Palestinian expression and movement.
             </li>
             <li>
-              <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl  font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Guarantee</span> the rights of students and staff to free expression and end the targeted repression of Palestine solidarity activism on campus.  Revoke the new SOAS protest policy.
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl  font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Guarantee</span> the rights of students and staff to free expression and end the targeted repression of Palestine solidarity activism on campus.  Revoke the new SOAS protest policy.
             </li>
             <li>
-              <span class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl  font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>⧨ </span>Advocate</span> for the UK government to implement an immediate arms embargo on Israel and to use all leverage to effect an immediate, unconditional, and permanent ceasefire
+              <span class="text-xl sm:text-2xl lg:text-3xl xl:text-4xl  font-bold text-red-600"><span class='text-3xl sm:text-4xl lg:text-5xl'>▼ </span>Advocate</span> for the UK government to implement an immediate arms embargo on Israel and to use all leverage to effect an immediate, unconditional, and permanent ceasefire
             </li>      
           </ol>
         </div>     
