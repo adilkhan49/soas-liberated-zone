0b3a0cfd715f6b0bb5a3ca82b9875c0cbb49349f
adil <adilkhan49@hotmail.com>
2025-01-15 15:18:48 -0600

    export signups as csv

--- backend/slz/admin.py
+++ backend/slz/admin.py
@@ -69,3 +69,21 @@ class CallToActionAdmin(admin.ModelAdmin):
 class SignUpAdmin(admin.ModelAdmin):
     list_display = ['name','email','affiliation','university']
     ordering = ['-created_on']
+    actions = ["export_as_csv"]
+    def export_as_csv(self, request, queryset):
+
+        field_names = ['name','email','affiliation','university','message']
+
+        response = HttpResponse(content_type='text/csv')
+        ts = get_timestamp()
+        response['Content-Disposition'] = f'attachment; filename=Sign Up List {ts}.csv'
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+    
+    export_as_csv.short_description = "Export Selected"
+
